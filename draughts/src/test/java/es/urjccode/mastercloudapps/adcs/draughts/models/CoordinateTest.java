96ff4d269e447cc3925ae5862569d2e9bd56eb89
Neofitos Koutsourais <nkoutsourais@gmail.com>
2019-11-17 16:17:42 +0100

    Prueba betweenDiagonal devuelve array coordenas

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/CoordinateTest.java
@@ -8,10 +8,10 @@ public class CoordinateTest {
 
     @Test
     public void testGivenTwoCoordinatesWhenBettweenDiagonalThenOk() {
-        assertEquals(new Coordinate(1, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0)));
-        assertEquals(new Coordinate(3, 1), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0)));
-        assertEquals(new Coordinate(3, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4)));
-        assertEquals(new Coordinate(1, 3), new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4)));
+        assertEquals(new Coordinate[] { new Coordinate(1, 1) }, new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 0)));
+        assertEquals(new Coordinate[] { new Coordinate(3, 1) }, new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 0)));
+        assertEquals(new Coordinate[] { new Coordinate(3, 3) }, new Coordinate(2, 2).betweenDiagonal(new Coordinate(4, 4)));
+        assertEquals(new Coordinate[] { new Coordinate(1, 3) }, new Coordinate(2, 2).betweenDiagonal(new Coordinate(0, 4)));
     }
 
     @Test
